bc483bd5c8330982833dbf8759ec0fbc24994acd
sebastian <sebastian bowen>
2021-06-11 03:24:58 -0600

    save commit

---
+++ client/src/components/ReactionForm/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const ReactionForm = ({ thoughtId }) => {
+    return (
+
+        <div>
+            <p className=" m-0">
+                Character Count: 0/280
+            </p>
+            <form className="flex-row justify-center justify-space-between-md align-stretch">
+                
+            </form>
+        </div>
+    )
+}
--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,10 +1,37 @@
 import React, { useState } from'react';
-//import ThoughtList from '../ThoughtList';
+import { useMutation } from '@apollo/client';
+import { ADD_THOUGHT } from'../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 
 const ThoughtForm =() => {
    const [thoughtText, setText] = useState('');
    const [characterCount, setCharacterCount] = useState(0);
+
+   const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+       update(cache, {data: { addThought } }) {
+
+        try { 
+           // read whats currently in the cache
+           const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+           // prepend the newest thought to the front of the array
+           cache.writeQuery({
+               query: QUERY_THOUGHTS,
+               data: { thoughts: [addThought, ...thoughts] }
+           });
+        }catch (e) {
+            console.error(e)
+        }
+
+        //  update me objects cache, appending new thought to the end of the array
+        const { me } = cache.readQuery({ query: QUERY_ME });
+        cache.writeQuery({
+            query: QUERY_ME,
+            data: {me: {...me, thoughts: [...me.thoughts, addThought] } }
+        });  
+       }
+   });
 
    // this handle change will keep track of chars in text box
    const handleChange = event => {
@@ -15,15 +42,27 @@ const ThoughtForm =() => {
    }
    const handleFormSubmit = async event => {
        event.preventDefault();
-       setText('')
-       setCharacterCount(0);
+      
+       try {
+           // add thought to database
+           await addThought({
+               variables: { thoughtText }
+           });
+
+           // clear form value
+           setText('');
+           setCharacterCount(0);
+       } catch (e) {
+           console.error(e)
+       }
    };
 
 // code below in <p> sets character maximun and will dwindle with each new char as well as turn red when maxed
     return (
         <div> 
-            <p className={ `m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+            <p className={ `m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
                 Character Count: {characterCount}/280
+                {error && <span className="ml-2"> Somthing Is Not Right</span>}
             </p>
             <form className="flex-row justify-center justify-space-between-md align-stretch"
             onSubmit={handleFormSubmit}
